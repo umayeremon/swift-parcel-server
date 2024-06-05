9cbe87af9107f63cba183df576232a7c0232b133
EMON <mamudmdemon@gmail.com>
2024-06-05 16:05:50 +0600

    install jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
